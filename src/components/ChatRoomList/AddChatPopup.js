99480f9bf47b1eb91526e2b0a3f4967f9688b5ce
unknown <checkpoint1209@gmail.com>
2021-01-21 11:09:28 +0900

    Added SelectedUserList

--- src/components/ChatRoomList/AddChatPopup.js
+++ src/components/ChatRoomList/AddChatPopup.js
@@ -1,7 +1,9 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 function AddChatPopup() {
+	const userDiv = useRef();
+	const nextButton = useRef();
 	const store = useSelector((state) => state);
 	const [selectedUsers, setSelectedUsers] = useState([]);
 	const [search, setSearch] = useState('');
@@ -12,9 +14,25 @@ function AddChatPopup() {
 		if (e.target !== checkbox) checkbox.checked = !checkbox.checked;
 
 		if (checkbox.checked) {
-			setSelectedUsers(selectedUsers.concat(userBox.id));
+			setSelectedUsers(selectedUsers.concat(userBox.data));
+
+			//새로운 노드를 만들고 거기에 요소를 붙임
+			const userIcon = document.createElement('div');
+			const userImg = e.currentTarget.childNodes[1].cloneNode(true);
+			const userName = e.currentTarget.childNodes[2].cloneNode(true);
+			console.log(userName);
+			userIcon.setAttribute('class', 'UserIcon');
+			userIcon.setAttribute('id', userBox.data);
+			userIcon.appendChild(userImg);
+			userIcon.appendChild(userName);
+
+			userDiv.current.appendChild(userIcon);
 		} else {
-			setSelectedUsers(selectedUsers.filter((item) => item !== userBox.id));
+			//제거
+			setSelectedUsers(selectedUsers.filter((item) => item !== userBox.data));
+
+			const userIcon = document.querySelector(`#${userBox.data}`);
+			userDiv.current.removeChild(userIcon);
 		}
 		console.log(selectedUsers);
 	};
@@ -24,13 +42,22 @@ function AddChatPopup() {
 		setSearch(e.currentTarget.value);
 	};
 
+	useEffect(() => {
+		//인원이 있을경우 버튼 비활성화
+		if (selectedUsers.length) {
+			nextButton.current.removeAttribute('disabled');
+		} else {
+			nextButton.current.setAttribute('disabled', true);
+		}
+	}, [selectedUsers]);
+
 	const link =
 		'https://upload.wikimedia.org/wikipedia/commons/c/c1/Lionel_Messi_20180626.jpg';
 
 	return (
 		<div className="AddChatPopup">
 			<h3>채팅할 사람 추가하기</h3>
-			<div className="SelectedUserList" />
+			<div className="SelectedUserList" ref={userDiv} />
 			<input className="UserSearchBox" onChange={searchUpdate} />
 			{store.user.friendsList
 				.filter((item) => item.userName.includes(search))
@@ -38,7 +65,7 @@ function AddChatPopup() {
 					<div
 						className="SimpleUserBox"
 						key={item._id}
-						id={item._id}
+						data={item._id}
 						onClick={userBoxClick}
 					>
 						<input type="checkbox" readOnly />
@@ -46,6 +73,9 @@ function AddChatPopup() {
 						<h4>{item.userName}</h4>
 					</div>
 				))}
+			<button className="CreateButton" ref={nextButton} disabled>
+				만들기
+			</button>
 		</div>
 	);
 }
