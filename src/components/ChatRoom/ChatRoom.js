5ab6dcac2e220c6dde24d20ec7f789faa9df949c
unknown <checkpoint1209@gmail.com>
2021-01-23 15:42:57 +0900

    Implemented Exit Room

--- src/components/ChatRoom/ChatRoom.js
+++ src/components/ChatRoom/ChatRoom.js
@@ -4,10 +4,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
 import { base_uri } from '../..';
 import { addPrevChatObjList } from '../../modules/room';
+import user from '../../modules/user';
 import { Websocket } from '../Websocket/WebSocket';
 import ChatBox from './ChatBox';
 
 function ChatRoom() {
+	const link =
+		'https://upload.wikimedia.org/wikipedia/commons/c/c1/Lionel_Messi_20180626.jpg';
 	const store = useSelector((state) => state);
 	const dispatch = useDispatch();
 
@@ -15,7 +18,6 @@ function ChatRoom() {
 	const inputBox = useRef();
 	const info = store.room.get(id);
 
-	console.log(store);
 	useEffect(() => {
 		const getChatRoom = async (from, amount) => {
 			const message = `${base_uri}/api/getRoomChat?roomId=${info._id}&from=${from}&amount=${amount}`;
@@ -23,10 +25,12 @@ function ChatRoom() {
 			dispatch(addPrevChatObjList(id, data));
 			info.chatLoaded = true;
 
-			// console.log(data);
+			console.log(data);
 		};
 		if (info.chatIdList.length && !info.chatLoaded) {
 			getChatRoom(info.chatIdList[info.chatIdList.length - 1], 50);
+		} else {
+			info.chatLoaded = true;
 		}
 	}, []);
 
@@ -42,30 +46,66 @@ function ChatRoom() {
 		inputBox.current.value = '';
 	};
 
+	const exitRoom = async () => {
+		const message = `${base_uri}/api/exitRoom`;
+		const { data } = await axios.post(message, {
+			roomId: id,
+			userId: store.user._id,
+		});
+		console.log(data);
+		window.location.href = `http://localhost:3000/#/chats`;
+	};
+
 	if (!info.chatLoaded) {
 		return <div className="ChatRoom">Loading...</div>;
 	}
 	return (
 		<div className="ChatRoom">
-			<div className="ChatRoomHeader">
-				<Link to="/chats">
-					<i className="fas fa-chevron-left" />
-				</Link>
-				<span className="ChatRoomName">{info.roomName}</span>
-				<i className="fas fa-bars" />
-			</div>
+			<div className="ChatRoomMain">
+				<div className="ChatRoomHeader">
+					<Link to="/chats">
+						<i className="fas fa-chevron-left" />
+					</Link>
+					<span className="ChatRoomName">{info.roomName}</span>
+					<i className="fas fa-bars" />
+				</div>
 
-			<div className="ChatDialogs">
-				{info.chatObjList.map((item) => (
-					<ChatBox info={item} userId={store.user._id} key={item._id} />
-				))}
-			</div>
+				<div className="ChatDialogs">
+					{info.chatObjList.map((item) => (
+						<ChatBox info={item} userId={store.user._id} key={item._id} />
+					))}
+				</div>
 
-			<div className="ChatBottom">
-				<i className="far fa-plus-square" />
-				<input className="ChatInput" ref={inputBox}></input>
-				<button className="ChatButton" onClick={sendChat}>
-					<i className="fas fa-arrow-right" />
+				<div className="ChatBottom">
+					<i className="far fa-plus-square" />
+					<input className="ChatInput" ref={inputBox}></input>
+					<button className="ChatButton" onClick={sendChat}>
+						<i className="fas fa-arrow-right" />
+					</button>
+				</div>
+			</div>
+			<div className="ChatRoomOption">
+				<h3>Chat Option</h3>
+				<h4>Users</h4>
+				{
+					<div className="SimpleUserBox">
+						<img className="SimpleUserBoxImg" src={link} width="100" />
+						<i className="fas fa-user" />
+						<h4>{store.user.userName}</h4>
+					</div>
+				}
+				{store.room
+					.get(String(id))
+					.userList.filter((item) => item._id !== store.user._id)
+					.map((item) => (
+						<div className="SimpleUserBox" key={item._id}>
+							<img className="SimpleUserBoxImg" src={link} width="100" />
+							<h4>{item.userName}</h4>
+						</div>
+					))}
+				<button className="InviteButton">Invite (notImplemented)</button>
+				<button className="Exit Room" onClick={exitRoom}>
+					Exit Room
 				</button>
 			</div>
 		</div>
--- src/components/ChatRoomList/ChatRoomList.js
+++ src/components/ChatRoomList/ChatRoomList.js
@@ -4,10 +4,11 @@ import ChatRoomBlock from './ChatRoomBlock';
 
 function ChatRoomList() {
 	const store = useSelector((state) => state);
+	console.log(store);
 	return (
 		<div className="ChatRoomList">
 			<h1>Chats</h1>
-			{/* sort function 추가하기~!!!!!!! */}
+			{/* sort function 추가하기!!!!!!! */}
 			{Array.from(store.room.entries()).map((item) => (
 				<ChatRoomBlock info={item[1]} key={item[1]._id} />
 			))}
